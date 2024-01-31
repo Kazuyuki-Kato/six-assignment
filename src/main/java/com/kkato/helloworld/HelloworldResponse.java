f65f9f8198b88c7cef4a72ee6540dc8c2dacf28f
unknown <ikko.makya@gmail.com>
2024-01-31 11:01:18 +0900

    deleteからpatchへの変更とJSONへの表示方法の変更

---
+++ src/main/java/com/kkato/helloworld/HelloworldResponse.java
@@ -0,0 +1,14 @@
+package com.kkato.helloworld;
+
+public class HelloworldResponse {
+    private String greeting;
+
+    public HelloworldResponse(String greeting) {
+        this.greeting = greeting;
+    }
+
+    public String getGreeting() {
+        return greeting;
+    }
+}
+
